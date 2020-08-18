9e760170506051c0d7d1d58db61251aeb90e2e09
Daniel Crowe <crowe.daniel@gmail.com>
2020-08-18 22:16:31 +0930

    Address codacy issues

--- visual_regression_tracker/__init__.py
+++ visual_regression_tracker/__init__.py
@@ -1,3 +1,28 @@
-# flake8: noqa
+"""
+Python SDK for Visual Regression Tracker
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Open source, self hosted solution for visual testing and managing results of visual testing.
+
+Basic usage:
+    from visual_regression_tracker import VisualRegressionTracker, Config, TestRun
+    config = Config(
+        apiUrl='http://localhost:4200',
+        project='Default project',
+        apiKey='tXZVHX0EA4YQM1MGDD',
+        branchName='develop',
+    )
+    with VisualRegressionTracker(config) as vrt:
+        vrt.track(TestRun(
+            name='Image name',
+            imageBase64=image,
+        ))
+"""
+
 from .types import Config, Build, TestRun, TestRunResult, TestRunStatus
 from .visualRegressionTracker import VisualRegressionTracker
+
+__all__ = [
+    'Config', 'Build', 'TestRun', 'TestRunResult', 'TestRunStatus',
+    'VisualRegressionTracker',
+]
--- visual_regression_tracker/visualRegressionTracker.py
+++ visual_regression_tracker/visualRegressionTracker.py
@@ -11,6 +11,10 @@ class VisualRegressionTracker:
     headers: dict = None
 
     def __init__(self, config: Config):
+        """Creates a new VisualRegressionTracker
+
+        :param config: The configuration to use.
+        """
         self.config = config
         self.headers = {'apiKey': self.config.apiKey}
 
