e73abb26835006a69bad71c32878fab803c90d6d
Daniel Crowe <crowe.daniel@gmail.com>
2020-09-14 20:37:20 +0930

    Address codacy issues

--- visual_regression_tracker/exceptions.py
+++ visual_regression_tracker/exceptions.py
@@ -2,22 +2,24 @@ from .types import TestRunStatus
 
 
 class VisualRegressionTrackerError(Exception):
+
     """An error occurred."""
     pass
 
 
 class ServerError(VisualRegressionTrackerError):
+
     """An error occurred on the VisualRegressionTracker server."""
     def __init__(self, response: dict, *args):
-        """
-        Initialises ServerError from server response
-        """
+        """Initialises ServerError from server response."""
         self.response = response
         super(ServerError, self).__init__(*args)
 
 
 class TestFailed(VisualRegressionTrackerError):
+
     """A visual test failed."""
     def __init__(self, status: TestRunStatus, *args):
+        """Initialises TestFailed error."""
         self.status = status
         super(TestFailed, self).__init__(*args)
