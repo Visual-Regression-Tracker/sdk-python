d3a4558d3af3ea7700bd0a731bb21c310e7420c6
Pavel Strunkin <pashidlos@gmail.com>
2020-09-15 08:57:39 +0200

    Update exceptions.py

--- visual_regression_tracker/exceptions.py
+++ visual_regression_tracker/exceptions.py
@@ -2,26 +2,26 @@ from .types import TestRunStatus
 
 
 class VisualRegressionTrackerError(Exception):
-
     """An error occurred."""
+
     pass
 
 
 class ServerError(VisualRegressionTrackerError):
-
     """An error occurred on the VisualRegressionTracker server."""
 
     def __init__(self, response: dict, *args):
         """Initialises ServerError from server response."""
+
         self.response = response
         super(ServerError, self).__init__(*args)
 
 
 class TestRunError(VisualRegressionTrackerError):
-
     """A visual test failed."""
 
     def __init__(self, status: TestRunStatus, *args):
         """Initialises TestRunError error."""
+
         self.status = status
         super(TestRunError, self).__init__(*args)
