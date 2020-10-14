378d2aa73981f301b23263414b899d0ba9924b1e
Pavel Strunkin <pashidlos@gmail.com>
2020-10-14 09:47:33 +0300

    Release 4.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='visual_regression_tracker',
-    version='4.2.0',
+    version='4.3.0',
     description='Open source, self hosted solution for visual testing '
                 'and managing results of visual testing.',
     long_description=open('README.md').read(),
